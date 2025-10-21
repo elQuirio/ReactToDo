88b2d905e9ce74d54e7a4c4b68d7e1856d97c817
Filippo Quirino <filippo.quirino@hotmail.it>
2025-10-21 23:44:52 +0200

    quick actions restyling

--- todo/src/components/statusBar.jsx
+++ todo/src/components/statusBar.jsx
@@ -1,16 +1,19 @@
 import { useSelector } from 'react-redux';
 import { selectActiveTodos, selectCompletedTodos, selectOverdueTodos } from '../selectors/todoSelectors';
-import { Search, ArrowUpAZ, ArrowDownZA } from "lucide-react";
+import { Search, ArrowUpAZ, ArrowDownZA, Minimize2, Maximize2 } from "lucide-react";
 
 export function StatusBar() {
     const activeTodos = useSelector(selectActiveTodos);
     const completedTodos = useSelector(selectCompletedTodos);
     const overdueTodos = useSelector(selectOverdueTodos);
 
+
+    // aggiungere cambio button con ordinamento e collapse expand
     return <div className="status-bar-mini">
             <span className='quick-actions-container'>
-                <Search className='search-button' size={18}/> 
-                <ArrowUpAZ className='sort-button' size={18}/>
+                <button className='search-button quick-actions-button'><Search className='search-icon' size={18}/></button>
+                <button className='sort-button quick-actions-button'><ArrowUpAZ className='sort-icon' size={18}/></button>
+                <button className='collapse-button quick-actions-button'><Minimize2 className='collapse-icon' size={18}/></button>
             </span>
             <span className='status-chip-container'>
                 <span className='status-chip'>Active: {activeTodos.length}</span>
