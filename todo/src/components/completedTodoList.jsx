4c27d58a048b5c3ac8f86b60627e73f0a8f20161
Filippo Quirino <filippo.quirino@hotmail.it>
2025-11-11 14:45:28 +0100

    minor bugfix and css reordering

--- todo/src/components/completedTodoList.jsx
+++ todo/src/components/completedTodoList.jsx
@@ -4,5 +4,5 @@ import { TodoItem } from "./todoItem";
 
 export default function CompletedTodoList() {
     const completedTodoList = useSelector(selectCompletedTodos);
-    return <div> {completedTodoList.map((td) => <TodoItem key={td.id} id={td.id} status={td.status} text={td.text} createdAt={td.createdAt} updatedAt={td.updatedAt} toBeCompletedAt={td.toBeCompletedAt} />)} </div>
+    return <div> {completedTodoList.map((td) => <TodoItem key={td.id} id={td.id} status={td.status} text={td.text} createdAt={td.createdAt} updatedAt={td.updatedAt} toBeCompletedAt={td.toBeCompletedAt} position={td.position} />)} </div>
 };
