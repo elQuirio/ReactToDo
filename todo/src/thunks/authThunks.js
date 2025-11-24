cbab8847d19b2c2898e1b16dcaa4411db0cfadff
Filippo Quirino <filippo.quirino@hotmail.it>
2025-11-24 20:43:39 +0100

    error response handling in login/registration thunks and component

--- todo/src/thunks/authThunks.js
+++ todo/src/thunks/authThunks.js
@@ -1,27 +1,43 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
 
 export const registerUser = createAsyncThunk('auth/registerUser', 
-    async ( credentials, {dispatch}) => {
-        const resp = await fetch('http://localhost:3000/api/auth/register', 
+    async ( credentials, {dispatch, rejectWithValue}) => {
+        try {
+            const resp = await fetch('http://localhost:3000/api/auth/register', 
             {   method:"POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(credentials)
              });
-        const respRegister = await resp.json();
-        console.log(respRegister);
-        return respRegister; // aggiungere dispatch allo store
-    }
-);
+            const respRegister = await resp.json();
+            console.log(resp);
+            if (!resp.ok) {
+                console.log('RESP not ok');
+                return rejectWithValue(respRegister.error);
+            }
+            console.log(respRegister);
+            return respRegister; // aggiungere dispatch allo store
+        }
+        catch (e) {
+            return rejectWithValue(e);
+        }
+});
 
 export const loginUser = createAsyncThunk('auth/loginUser',
-    async (credentials , {dispatch}) => {
-        const res = await fetch('http://localhost:3000/api/auth/login', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(credentials)
-        });
-        const respLogin = await res.json();
-        console.log(respLogin);
-        return respLogin; // aggiungere dispatch allo store
+    async (credentials , {dispatch, rejectWithValue}) => {
+        try {
+            const resp = await fetch('http://localhost:3000/api/auth/login', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(credentials)
+            });
+            const respLogin = await resp.json();
+            if (!resp.ok) {
+                console.log('RESP not ok');
+                return rejectWithValue(respLogin.error);
+            }
+            console.log(respLogin);
+            return respLogin; // aggiungere dispatch allo store
+        } catch (e) {
+            return rejectWithValue(e);
     }
-);
+});
