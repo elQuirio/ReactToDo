8d2dce415ac1a603146eac6360f15dded723cd7e
Filippo Quirino <filippo.quirino@hotmail.it>
2026-02-04 20:55:19 +0100

    added error bubble style to vaidation messages on login/registration and styling to serverError popup

--- todo/src/components/loginFormLogin.jsx
+++ todo/src/components/loginFormLogin.jsx
@@ -6,10 +6,23 @@ export function LoginFormLogin({fieldErrors, handleEmailOnChange, handlePassword
 
 
     return (<div className="login-form-container">
-                <input type="email" placeholder="Email..." onChange={(e)=> handleEmailOnChange(e)} className="login-form-input"/>
-                {fieldErrors.email && <div className="login-input-error-text">{fieldErrors.email}</div>}
-                <input type="password" placeholder="Password..." onChange={(e) => handlePasswordOnChange(e)} className="input-password login-form-input"/>
-                {fieldErrors.password && <div className="login-input-error-text">{fieldErrors.password}</div>}
+                <div>
+                    <div className="login-form-input-wrapper">
+                        <input type="email" placeholder="Email..." onChange={(e)=> handleEmailOnChange(e)} className="login-form-input"/>
+                        {fieldErrors.email && <div className="login-form-input-bubble">
+                            {fieldErrors.email}
+                            <span className="error-arrow"/>
+                        </div>}
+                    </div>
+                    <div className="login-form-input-wrapper">
+                        <input type="password" placeholder="Password..." onChange={(e) => handlePasswordOnChange(e)} className="input-password login-form-input"/>
+                        {fieldErrors.password && <div className="login-form-input-bubble">
+                            {fieldErrors.password}
+                            <span className="error-arrow"/>
+                        </div>}
+                    </div>
+                </div>
+
                 <button type="submit" className="login-form-button" disabled={isAuthLoading}>
                     {isAuthLoading ? <><div>Logging in...</div><div className="button-spinner"></div></> : <div>Login</div>}
                 </button>
--- todo/src/components/loginFormRegister.jsx
+++ todo/src/components/loginFormRegister.jsx
@@ -7,12 +7,30 @@ export function LoginFormRegister({fieldErrors, handleEmailOnChange, handlePassw
 
 
     return <div className="login-form-container">
-                <input type="email" placeholder="Email..." onChange={(e)=> handleEmailOnChange(e)} className="login-form-input"/>
-                {fieldErrors.email && <div className="login-input-error-text">{fieldErrors.email}</div>}
-                <input type="password" placeholder="Password..." onChange={(e) => handlePasswordOnChange(e)} className="input-password login-form-input"/>
-                {fieldErrors.password && <div className="login-input-error-text">{fieldErrors.password}</div>}
-                <input type="password" placeholder="Confirm password..." className={fieldErrors.password ? 'password-error login-form-input' : 'input-password login-form-input'} onChange={(e) => handleConfirmPasswordChange(e)}/>
-                {fieldErrors.confirmPassword && <div className="login-input-error-text">{fieldErrors.confirmPassword}</div>}
+                <div>
+                    <div className="login-form-input-wrapper">
+                        <input type="email" placeholder="Email..." onChange={(e)=> handleEmailOnChange(e)} className="login-form-input"/>
+                        {fieldErrors.email && <div className="login-form-input-bubble">
+                                                    {fieldErrors.email}
+                                                    <span className='error-arrow'/>
+                                                </div>}
+                    </div>
+                    <div className="login-form-input-wrapper">
+                        <input type="password" placeholder="Password..." onChange={(e) => handlePasswordOnChange(e)} className="input-password login-form-input"/>
+                        {fieldErrors.password && <div className="login-form-input-bubble">
+                                                    {fieldErrors.password}
+                                                    <span className='error-arrow'/>
+                                                </div>}
+                    </div>
+                    <div className="login-form-input-wrapper">
+                        <input type="password" placeholder="Confirm password..." className={fieldErrors.password ? 'password-error login-form-input' : 'input-password login-form-input'} onChange={(e) => handleConfirmPasswordChange(e)}/>
+                        {fieldErrors.confirmPassword && <div className="login-form-input-bubble">
+                                                            {fieldErrors.confirmPassword}
+                                                            <span className='error-arrow'/>
+                                                        </div>}
+                    </div>
+                </div>
+
                 <button type='submit' className="login-form-button">
                     {isAuthLoading ? <><div>Registering...</div><div className='button-spinner'></div></> : <div>Confirm</div>}
                 </button>
