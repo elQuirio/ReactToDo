f3e50f9c94c6da90884e99a189ef77a9b235670b
Filippo Quirino <filippo.quirino@hotmail.it>
2026-02-05 21:43:32 +0100

    reorganized css variables for all login components and split loginForm sub components into loginFormInput components

--- todo/src/components/loginFormLogin.jsx
+++ todo/src/components/loginFormLogin.jsx
@@ -1,26 +1,15 @@
 import { selectAuthLoading } from "../selectors/authSelector";
 import { useSelector } from "react-redux";
+import { LoginFormInput } from "./loginFormInput";
 
 export function LoginFormLogin({fieldErrors, handleEmailOnChange, handlePasswordOnChange, setRegistrationMode}) {
     const isAuthLoading = useSelector(selectAuthLoading);
 
-
+    
     return (<div className="login-form-container">
                 <div>
-                    <div className="login-form-input-wrapper">
-                        <input type="email" placeholder="Email..." onChange={(e)=> handleEmailOnChange(e)} className="login-form-input"/>
-                        {fieldErrors.email && <div className="login-form-input-bubble">
-                            {fieldErrors.email}
-                            <span className="error-arrow"/>
-                        </div>}
-                    </div>
-                    <div className="login-form-input-wrapper">
-                        <input type="password" placeholder="Password..." onChange={(e) => handlePasswordOnChange(e)} className="input-password login-form-input"/>
-                        {fieldErrors.password && <div className="login-form-input-bubble">
-                            {fieldErrors.password}
-                            <span className="error-arrow"/>
-                        </div>}
-                    </div>
+                    <LoginFormInput fieldError={fieldErrors.email} inputType={'email'} placeholder={'Email...'} onChange={handleEmailOnChange} />
+                    <LoginFormInput fieldError={fieldErrors.password} inputType={'password'} placeholder={'Password...'} onChange={handlePasswordOnChange} />
                 </div>
 
                 <button type="submit" className="login-form-button" disabled={isAuthLoading}>
