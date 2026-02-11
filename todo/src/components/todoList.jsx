2370db67cfe70bf657d97a9d8c8ad6115464ec27
Filippo Quirino <filippo.quirino@hotmail.it>
2026-02-11 21:07:20 +0100

    feature: sticky input box positioning and auto-scroll behavior

--- todo/src/components/todoList.jsx
+++ todo/src/components/todoList.jsx
@@ -4,17 +4,29 @@ import { selectUiTodos } from "../selectors/uiSelectors";
 import { selectViewMode } from '../selectors/preferencesSelector';
 import { TodoItem } from "./todoItem";
 import { selectSearchBtnToggled } from "../selectors/uiSelectors";
-
+import { useRef, useEffect } from "react";
 
 export default function TodoList() {
     const activeTodoSelector = useSelector( selectActiveTodos );
     const expandedTodos = useSelector( selectUiTodos );
     const searchButtonActive = useSelector(selectSearchBtnToggled);
     const searchedTodos = useSelector(selectSearchedVisibleTodo);
-    let currentViewMode = useSelector(selectViewMode);
     const allTodos = useSelector(selectTodos);
-
+    const prevCountRef = useRef(allTodos.length);
+    let currentViewMode = useSelector(selectViewMode);
     let todoList = [];
+
+    useEffect( () => {
+        const prev = prevCountRef.current;
+        const next = allTodos.length;
+
+        if (next > prev) {
+            window.scrollTo({top: document.documentElement.scrollHeight, behavior: "auto"});
+        }
+
+        prevCountRef.current = next;
+
+    }, [allTodos.length]);
 
     if (searchButtonActive) {
         todoList = searchedTodos;
